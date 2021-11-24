fc4c7f4382dfc6f9f8c33f1aac5a1c31affef1bc
Nicogarciablanco <nicogarciablanco99@gmail.com>
2021-11-24 11:51:47 -0300

    imagenes + migración del sitio de transportes

--- app.js
+++ app.js
@@ -5,7 +5,11 @@ var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+var nosotrosRouter = require('./routes/nosotros'); //nosotros.js
+var serviciosRouter = require('./routes/servicios'); //servicios.js
+var galeriaRouter = require('./routes/galeria'); //galeria.js
+var novedadesRouter = require('./routes/novedades'); //novedades.js
+var contactoRouter = require('./routes/contacto'); //contacto.js
 
 var app = express();
 
@@ -19,8 +23,16 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
+//app.get('/nosotros', function(req,res,next){
+  //res.send('Hola Curso')
+//})
+
 app.use('/', indexRouter);
-app.use('/users', usersRouter);
+app.use('/nosotros', nosotrosRouter);
+app.use('/servicios', serviciosRouter);
+app.use('/galeria', galeriaRouter);
+app.use('/novedades', novedadesRouter);
+app.use('/contacto', contactoRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
