1d57bb77a1f6d099c55dc44b2b67bff80b73d0a6
Nicogarciablanco <nicogarciablanco99@gmail.com>
2021-12-01 11:57:21 -0300

    login funcionamiento + sesiones

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
 require('dotenv').config(); //para que cargue los datos del .env
+var session = require('express-session');
 
 var indexRouter = require('./routes/index');
 var nosotrosRouter = require('./routes/nosotros'); //nosotros.js
@@ -12,7 +13,8 @@ var serviciosRouter = require('./routes/servicios'); //servicios.js
 var galeriaRouter = require('./routes/galeria'); //galeria.js
 var novedadesRouter = require('./routes/novedades'); //novedades.js
 var contactoRouter = require('./routes/contacto'); //contacto.js
-var loginRouter  = require('./routes/admin/login'); //login.js
+var loginRouter = require('./routes/admin/login'); //admin/login.js
+var adminNovedadesRouter = require('./routes/admin/novedades') //admin/novedades.js
 
 var app = express();
 
@@ -22,12 +24,35 @@ app.set('view engine', 'hbs');
 
 app.use(logger('dev'));
 app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
+app.use(express.urlencoded({
+  extended: false
+}));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
+app.use(session({
+  secret: '01234567899876543210',
+  resave: false,
+  saveUninitialized: true,
+  cookie: {maxAge: 60000}
+}))
+
+secured = async function (req, res, next) {
+  try {
+    console.log(req.session.id_usuario);
+
+    if (req.session.id_usuario) {
+      next()
+    } else {
+      res.redirect('/admin/login')
+    }
+  } catch (error) {
+    console.log(error)
+  }
+} //cierra secured
+
 //app.get('/nosotros', function(req,res,next){
-  //res.send('Hola Curso')
+//res.send('Hola Curso')
 //})
 
 app.use('/', indexRouter);
@@ -37,14 +62,15 @@ app.use('/galeria', galeriaRouter);
 app.use('/novedades', novedadesRouter);
 app.use('/contacto', contactoRouter);
 app.use('/admin/login', loginRouter);
+app.use('/admin/novedades', secured, adminNovedadesRouter)
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
+app.use(function (req, res, next) {
   next(createError(404));
 });
 
 // error handler
-app.use(function(err, req, res, next) {
+app.use(function (err, req, res, next) {
   // set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
--- routes/admin/login.js
+++ routes/admin/login.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var router = express.Router();
+var usuariosModel = require('./../../models/usuariosModels');
 
 /* recibir y mostrar la vista de admin/login */
 router.get('/', function(req,res,next){
@@ -8,7 +9,29 @@ router.get('/', function(req,res,next){
     })
 });
 
+router.post('/', async function(req,res,next){
+    try{
 
+        var usuario = req.body.usuario; //nicolas
+        var password = req.body.password; //1234
+
+        var data = await usuariosModel.getUserAndPassword(usuario,password)
+
+        if(data != undefined){
+            req.session.id_usuario = data.id;
+            req.session.nombre = data.usuario;
+            
+            res.redirect('/admin/novedades');
+        }else{
+            res.render('admin/login',{
+                layout: 'admin/layout',
+                error: true
+                })
+        }
+    }catch(error){
+        console.log(error);
+    }
+})
 
 
 module.exports = router;
