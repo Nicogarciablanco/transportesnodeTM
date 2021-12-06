bd81bfd17bc734258b6d99ce56bf036ded0a6548
Nicogarciablanco <nicogarciablanco99@gmail.com>
2021-12-06 11:01:40 -0300

    agregar novedades

--- routes/admin/novedades.js
+++ routes/admin/novedades.js
@@ -23,5 +23,29 @@ router.get('/agregar', function (req,res,next){
     });
 });
 
+router.post('/agregar', async (req,res,next) =>{
+    try{
+        console.log(req.body);
+
+        if (req.body.titulo != "" && req.body.subtitulo != "" && req.body.cuerpo != "" ){
+            await novedadesModel.insertNovedad(req.body);
+            res.redirect('/admin/novedades');
+        } else {
+            res.render('admin/agregar',{
+                layout:'admin/layout',
+                error:true,
+                message:'Todos los campos son requeridos'
+            })
+        }
+        
+    } catch (error){
+        console.log(error);
+        res.render('admin/agregar', {
+          layout: 'admin/layout',
+          error:true,
+          message: 'No se cargo la novedad'
+        })
+    }
+  })
 
 module.exports = router
