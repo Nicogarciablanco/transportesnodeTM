4803d35825b3c63eeb7ef09d29450496dff15352
Nicogarciablanco <nicogarciablanco99@gmail.com>
2021-12-06 13:23:43 -0300

    agregamos borrar

--- models/novedadesModels.js
+++ models/novedadesModels.js
@@ -17,7 +17,13 @@ async function insertNovedad(obj){
     }
 }
 
-module.exports = { getNovedades, insertNovedad }
+async function deleteNovedadByID(id){
+    var query ='delete from novedades where id= ?';
+    var rows = await pool.query(query,[id]);
+    return rows;
+}
+
+module.exports = { getNovedades, insertNovedad, deleteNovedadByID }
 
 
 
